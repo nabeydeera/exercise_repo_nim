368f0973f0bdb5c571a287835496051f83ba81df
nimekatest <nimeka.abeydeera@gmail.com>
2025-04-09 16:12:46 -0400

    Updated response code and added getLocation to classes

--- src/test/java/test/CheckResponseCode.java
+++ src/test/java/test/CheckResponseCode.java
@@ -6,26 +6,33 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+
 import util.BrowserNavigation;
 
 public class CheckResponseCode {
 	static BrowserNavigation browser;
 	static WebDriver driver;
 
-	public static void main(String[] args) throws MalformedURLException, IOException {
+	public static void main(String[] args) throws MalformedURLException, WebDriverException {
 		try {
 			browser = new BrowserNavigation();
-			String url = "https://accounts.lambdates.com/register";
+			String url = "https://accounts.lambdatest.com/register";
 			browser.openbrowser("Chrome", url);
 			
 			HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-			connection.setRequestMethod("GET");
+			//connection.setRequestMethod("GET");
 			connection.connect();
 			
 
-//get the response code
+			//get the response code
 			int responseCode = connection.getResponseCode();
+			String requestMethod = connection.getRequestMethod();
+			URL getUrl = connection.getURL();
+			
 			System.out.println("Response code: " + responseCode);
+			System.out.println("Request Method: "+requestMethod);
+			System.out.println("URL:"+getUrl);
 
 			if (responseCode ==200) {
 				System.out.println("Page Loaded successfully");
--- src/test/java/test/DragandDropMoveTo.java
+++ src/test/java/test/DragandDropMoveTo.java
@@ -7,6 +7,7 @@ import java.awt.Robot;
 import java.awt.event.InputEvent;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -16,8 +17,8 @@ import util.BrowserNavigation;
 public class DragandDropMoveTo {
 
 	static WebDriver driver;
-	 static BrowserNavigation browser;
-	 
+	static BrowserNavigation browser;
+
 	//Using Action class dragAndDrop method
 	/*
 	 * public static void main(String[] args) throws InterruptedException { browser
@@ -38,32 +39,44 @@ public class DragandDropMoveTo {
 	 * 
 	 * Thread.sleep(3000); browser.quitBrowser(); }
 	 */	 
-	
-	 //Using robot class Key presses
-		@SuppressWarnings("deprecation")
-		public static void main(String[] args) throws InterruptedException, AWTException {
-			  browser = new BrowserNavigation();
-			  browser.openbrowser("Chrome", "https://jqueryui.com/droppable/");
-			  driver = browser.getDriver();
 
-			  Robot robot = new Robot(); 
-			// Move the mouse to the element 
-			robot.mouseMove(508,520);
-			 // Press mouse button 
-			  robot.mousePress(InputEvent.BUTTON1_MASK); 
-			// Move the mouse to a new location (dragging) 
-			robot.mouseMove(670, 570);  
-			// Release the mouse button
-			robot.mouseRelease(InputEvent.BUTTON1_MASK);
-			  
-			  Thread.sleep(3000);
-			  
-			  driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']"))); 
-			  WebElement drop = driver.findElement(By.xpath("//div[@id='droppable']"));
-			
-			  assertEquals("Dropped!", drop.getText());
-			  browser.quitBrowser();
+	//Using robot class Key presses
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws InterruptedException, AWTException {
+		browser = new BrowserNavigation();
+		browser.openbrowser("Chrome", "https://jqueryui.com/droppable/");
+		driver = browser.getDriver();
 
-		}
-	
+
+		Robot robot = new Robot(); 
+		// Move the mouse to the element 
+		robot.mouseMove(508,520);
+		// Press mouse button 
+		robot.mousePress(InputEvent.BUTTON1_MASK); 
+
+		WebElement pageFrame = driver.findElement(By.xpath("//iframe[@class='demo-frame']"));
+		Point point1 = pageFrame.getLocation();
+
+		driver.switchTo().frame(pageFrame); 
+		WebElement drop = driver.findElement(By.xpath("//div[@id='droppable']"));
+		Point point2 = drop.getLocation();
+		//		System.out.println("Frame Element X location:"+point1.x);
+		//		System.out.println("Frame Element Y location:"+point1.y);
+		//		System.out.println("Dropped Element X location:"+point2.x);
+		//		System.out.println("Dropped Element Y location:"+point2.y);
+		//		System.out.println(point1.x+point2.x+125);
+		//		System.out.println(point1.y+point2.y+200);
+
+		// Move the mouse to a new location (dragging) 
+		robot.mouseMove(point1.x+point2.x+125, point1.y+point2.y+200);  
+		// Release the mouse button
+		robot.mouseRelease(InputEvent.BUTTON1_MASK);
+
+		Thread.sleep(3000);
+
+		assertEquals("Dropped!", drop.getText());
+		browser.quitBrowser();
+
+	}
+
 }
