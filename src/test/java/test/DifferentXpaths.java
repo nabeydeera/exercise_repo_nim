05e346b542504393bda4a30e0560c5c92df8bec6
nimekatest <nimeka.abeydeera@gmail.com>
2025-04-10 15:49:45 -0400

    practiced xpath and css with findBy

--- src/test/java/test/DifferentXpaths.java
+++ src/test/java/test/DifferentXpaths.java
@@ -1,58 +1,51 @@
 package test;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.SendKeysAction;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 import util.BrowserNavigation;
 
 public class DifferentXpaths {
-	static BrowserNavigation browser;
-	static WebDriver driver;
-	
+	BrowserNavigation browser;
+	WebDriver driver;
+
+	@CacheLookup
 	@FindBy(xpath = "//h2[text()='Get started for free']") WebElement headerTxt;
+	@CacheLookup
 	@FindBy(xpath = "//span[text()='Sign up with Google']") WebElement btnGoogle;
+	@CacheLookup
 	@FindBy(xpath = "//span[text()='Sign up with Github']") WebElement btnGithub;
-	@FindBy(xpath = "//input[@id='email']") WebElement txtEmail;
-	@FindBy(xpath = "//span[text()='Show']")
-	WebElement txtShowHide;
-	
-	
-	
-//	public DifferentXpaths(WebDriver driver) {
-//		this.driver = driver;
-//		PageFactory.initElements(driver, this);
-//	}
+	@CacheLookup
+	//@FindBy(xpath = "//input[@id='email']") WebElement txtEmail;
+	@FindBy(css = "input#email") WebElement txtEmail;
+	@CacheLookup
+	@FindBy(xpath = "//span[text()='Show']") WebElement txtShowHide;
+	@CacheLookup
+	@FindBy(css = "div.leading-5") WebElement signUpText; //long text that was divided into multiple tags
+	@CacheLookup
+	@FindBy(css = "button[type='submit']") WebElement btnSignUp;
 
-	
-	@Test
-	public void signUp() {
-		System.out.println(headerTxt.getText());
+	public DifferentXpaths(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 
-	@Test
-	public void assertTest() throws Exception {
-		 	
-		
-		
-	
-			SoftAssert softAssert = new SoftAssert();
-			softAssert.assertEquals(driver.getTitle(), "Sign up for free | Cross Browser Testing Tool | LambdaTest", "Assertion message");
-			//softAssert.assertEquals(headerTxt.getText(), "Get started for free");
-		
-			String headerText = driver.findElement(By.xpath("//h2[text()='Get started for free']")).getText();
-			System.out.println("header text:"+headerText);
-			
-			softAssert.assertAll();
-			Thread.sleep(3000);
-			
-			browser.quitBrowser();
-			
-		
+	public String sectionHeading() {
+		String headerName = headerTxt.getText();
+		return headerName;
+	}
+
+	public String enterEmail() {
+		String email = "test@email.com";
+		txtEmail.sendKeys(email);		
+		return email;
 	}
 	
+	public String signUpBtn() {
+		String signUp = btnSignUp.getText();
+		return signUp;
+	}
 }
