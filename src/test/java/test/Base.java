52c2ec9f828eb470d939a6824c813edc433ddc3f
nabeydeera <nimeka.abeydeera@gmail.com>
2025-06-10 14:33:02 -0400

    Update Base.java - removed the added println statement

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -27,7 +27,6 @@ public class Base{
 		browser = new BrowserNavigation();
 		browser.openbrowser("Chrome", "https://www.bravenet.com/login");
 		driver = browser.getDriver();
-		system.out.println("Testing the rebase with local");
 	}
 
 	@AfterSuite
